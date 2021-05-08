7e679ce30fe23fa1b1e97007c28e34a86230ecdb
Lucas Ramos <lucasramosgab90@gmail.com>
2021-05-08 10:55:08 -0300

    Implementando Feign e removendo o Template Rest

--- hr-payroll/src/main/java/com/udemy/payroll/services/paymentService.java
+++ hr-payroll/src/main/java/com/udemy/payroll/services/paymentService.java
@@ -1,31 +1,21 @@
 package com.udemy.payroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import com.udemy.payroll.entities.Payment;
 import com.udemy.payroll.entities.Worker;
+import com.udemy.payroll.feignclientes.WorkerFeignClient;
 
 @Service
 public class paymentService {
-
-	@Value("${hr-worker.host}")
-	private String workerHost;
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 	
 	public Payment getPayment(Long workerId, int days) {
-		Map<String, String> uriVaribleMap = new HashMap<String, String>();
-		uriVaribleMap.put("id", workerId.toString());
 		
-		Worker worker = restTemplate.getForObject(workerHost+ "/workers/{id}",
-				Worker.class,uriVaribleMap);
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 		
 	}
