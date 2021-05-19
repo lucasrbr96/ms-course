4b8e46c7a8a6dd4ac74d100130ae232844b95d0e
Lucas Ramos <lucasramosgab90@gmail.com>
2021-05-19 09:56:12 -0300

    Tolerancia com Hystrix

--- hr-payroll/src/main/java/com/udemy/payroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/udemy/payroll/HrPayrollApplication.java
@@ -2,6 +2,7 @@ package com.udemy.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
--- hr-payroll/src/main/java/com/udemy/payroll/resource/PaymentResource.java
+++ hr-payroll/src/main/java/com/udemy/payroll/resource/PaymentResource.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.udemy.payroll.entities.Payment;
 import com.udemy.payroll.services.paymentService;
 
@@ -17,9 +18,15 @@ public class PaymentResource {
 	@Autowired
 	private paymentService service;
 	
+	@HystrixCommand(fallbackMethod = "findByIdAlternative")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> findById(@PathVariable("workerId") Long id, @PathVariable("days") Integer days){
 		Payment payment = service.getPayment(id, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<Payment> findByIdAlternative(Long id, Integer days){
+		Payment payment = new Payment("Brann", 400.0, days);
 		return ResponseEntity.ok(payment);
 	}
 	
