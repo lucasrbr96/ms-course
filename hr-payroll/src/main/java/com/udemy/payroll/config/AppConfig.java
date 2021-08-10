a5999788bbf0add5343821abf33a55be19f8f0d6
Lucas Ramos <lucasramosgab90@gmail.com>
2021-08-10 12:28:56 -0300

    Login com JWT

--- hr-payroll/src/main/java/com/udemy/payroll/config/AppConfig.java
+++ hr-payroll/src/main/java/com/udemy/payroll/config/AppConfig.java
@@ -1,8 +1,6 @@
 package com.udemy.payroll.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
